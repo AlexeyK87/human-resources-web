743234c8984deafc0ff4bf47b12ee9384db2203e
Alexey Korban <ldwx@mail.ru>
2020-08-04 19:58:40 +0500

    Initial commit

---
+++ src/main/java/ru/ldwx/humanresourcesweb/HumanResourcesWebApplication.java
@@ -0,0 +1,13 @@
+package ru.ldwx.humanresourcesweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourcesWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ldwx/humanresourcesweb/HumanResourcesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ldwx.humanresourcesweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
