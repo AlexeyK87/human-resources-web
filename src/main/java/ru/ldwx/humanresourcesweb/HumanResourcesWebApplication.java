2887c37015e579d19c7a8b5f0460f1bd5fb5f852
Alexey Korban <ldwx@mail.ru>
2020-08-08 16:18:16 +0500

    Fix url

--- src/main/java/ru/ldwx/humanresourcesweb/HumanResourcesWebApplication.java
+++ src/main/java/ru/ldwx/humanresourcesweb/HumanResourcesWebApplication.java
@@ -2,12 +2,18 @@ package ru.ldwx.humanresourcesweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HumanResourcesWebApplication {
+public class HumanResourcesWebApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HumanResourcesWebApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HumanResourcesWebApplication.class);
+	}
 }
