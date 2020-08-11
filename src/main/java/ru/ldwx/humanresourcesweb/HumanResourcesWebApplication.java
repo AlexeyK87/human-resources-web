c3d07a6c96a1cd5475c564a9543c51a49fe0ea43
Alexey Korban <ldwx@mail.ru>
2020-08-11 20:40:35 +0500

    Redirect errors to index

--- src/main/java/ru/ldwx/humanresourcesweb/HumanResourcesWebApplication.java
+++ src/main/java/ru/ldwx/humanresourcesweb/HumanResourcesWebApplication.java
@@ -3,10 +3,17 @@ package ru.ldwx.humanresourcesweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class HumanResourcesWebApplication  extends SpringBootServletInitializer {
+public class HumanResourcesWebApplication  extends SpringBootServletInitializer implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HumanResourcesWebApplication.class, args);
@@ -15,5 +22,19 @@ public class HumanResourcesWebApplication  extends SpringBootServletInitializer 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(HumanResourcesWebApplication.class);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/notFound").setViewName("forward:/");
+	}
+
+
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
+		return container -> {
+			container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,
+					"/notFound"), new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/notFound"));
+		};
 	}
 }
